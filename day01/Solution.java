d91ecb95deeab479e9ec5869d219ff9bac8fd731
UniK <unik@users.noreply.github.com>
2023-12-02 10:46:03 +0100

    Day01 implement the solution

--- day01/Solution.java
+++ day01/Solution.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static java.lang.System.out;
@@ -13,11 +14,27 @@ public class Solution {
     public static void main(String... args) {
         out.println("Day one");
 
-        Path path = Paths.get("day01/example.txt");
+        List<String> calibrationValueList = List.of();
+
+        Path path = Paths.get("day01/input.txt");
         try (Stream<String> lines = Files.lines(path)) {
-            lines.forEach(out::println);
+            calibrationValueList = lines.map(line -> line.replaceAll("\\D+", ""))
+                    .map(line -> {
+                                char firstLetter = line.charAt(0);
+                                char lastLetter = line.charAt(line.length() - 1);
+                                return "%s%s".formatted(firstLetter, lastLetter);
+                            }
+                    )
+                    .toList();
         } catch (IOException e) {
             out.println("Error reading file");
         }
+
+        Integer sum = calibrationValueList.stream()
+                .map(Integer::parseInt)
+                .reduce(Integer::sum)
+                .orElse(-1);
+
+        out.println("Answer: " + sum);
     }
 }
